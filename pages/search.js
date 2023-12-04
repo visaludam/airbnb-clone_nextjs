ddd5cd495caa43937e71d66fc964ebec7a5f751a
visaludam <udamvisal1901@gmail.com>
2023-12-04 17:07:51 +0900

    added card info

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import InfoCard from "@/components/InfoCard";
-import Map from "@/components/Map";
 import { format } from "date-fns";
 import { useRouter } from "next/router";
 import React from "react";
